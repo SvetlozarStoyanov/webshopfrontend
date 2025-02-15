fa26aba315121263a20016ba9c95a8bf1f1021f3
SvetlozarStoyanov <svetlio214@gmail.com>
2025-02-15 15:37:48 +0200

    Added links for login/logout

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  constructor(private readonly authService: AuthService) {
 
+  }
+  ngOnInit(): void {
+    this.authService.authStatus$.subscribe(res => {
+      this.isLoggedIn = res;
+    });
+  }
+
+  logout() {
+    this.authService.logout().subscribe(res => {
+      window.location.reload();
+    });
+  }
 }
